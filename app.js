9094b1c50b903bc649f8146b2b52988ad7d495c4
gvialpai <vialpg@gmail.com>
2016-01-01 15:46:13 +0100

    BU done

--- app.js
+++ app.js
@@ -35,22 +35,26 @@ $( document ).ready(function() {
         $("#mitModal").modal();
     });
 
+    $("#bubutton").click(function(){
+        $("#buModal").modal();
+    });
+
     $(function() {
     // OPACITY OF BUTTON SET TO 0%
     $(".roll").css("opacity","0");
-    
+
     // ON MOUSE OVER
     $(".roll").hover(function () {
-     
+
     // SET OPACITY TO 70%
     $(this).stop().animate({
       opacity: .7
     }, "slow");
   },
-  
+
     // ON MOUSE OUT
     function () {
-     
+
     // SET OPACITY BACK TO 50%
     $(this).stop().animate({
       opacity: 0
